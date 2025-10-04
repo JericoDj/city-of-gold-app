59cf7656c76f1fdac0b0f2b486538acbed1209c6
jericodejesus <dejesusjerico528@gmail.com>
2025-10-04 15:55:05 +0800

    Added online or offline functionality for database

--- city-of-gold-be/src/db/db.ts
+++ city-of-gold-be/src/db/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",        
   user: process.env.DB_USER || "user",      
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "password",
   database: process.env.DB_NAME || "city_of_gold",
   port: Number(process.env.DB_PORT) || 3306, 
   timezone: "+08:00",

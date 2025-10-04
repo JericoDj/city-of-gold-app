d7c5e33039c22831a5e83c4299ca0603a09c6dc7
jericodejesus <dejesusjerico528@gmail.com>
2025-10-04 16:16:33 +0800

    Added online or offline functionality for database

--- city-of-gold-be/src/db/db.ts
+++ city-of-gold-be/src/db/db.ts
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",        
-  user: process.env.DB_USER || "user",      
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "city_of_gold",
+  host: process.env.DB_HOST || "bdgx8znj66odcc022qtw-mysql.services.clever-cloud.com",        
+  user: process.env.DB_USER || "u8mpaehh5lm24bzn",      
+  password: process.env.DB_PASSWORD || "4tBnzDNUzh3bIuYYaAoj",
+  database: process.env.DB_NAME || "bdgx8znj66odcc022qtw",
   port: Number(process.env.DB_PORT) || 3306, 
   timezone: "+08:00",
 });
